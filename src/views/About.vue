142bd61b237a86a41932ee9e55fe7bbedc46b2ee
Niaz Estefaie <estefaie.niaz@gmail.com>
2020-12-23 17:51:31 +0330

    add new features !

---
+++ src/views/About.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="about relative">
+    <transition-group name="fade" tag="div">
+      <div v-for="i in [currentIndex]" :key="i" class="p-20 text-center-web">
+        <img :src="currentImg" class="w-9/12 h-96 object-cover" />
+      </div>
+    </transition-group>
+    <a
+      class="prev z-10 text-gray-200 absolute top-64 text-7xl left-16"
+      @click="prev"
+      href="#"
+      >&#10094;</a
+    >
+    <a
+      class="next z-10 text-gray-200 absolute top-64 text-7xl right-16"
+      @click="next"
+      href="#"
+      >&#10095;</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      images: [
+        "https://source.unsplash.com/random",
+        "https://source.unsplash.com/collection/190727/1600x900",
+        "https://source.unsplash.com/WLUHO9A_xik/1600x900",
+        "https://source.unsplash.com/user/erondu/daily",
+        "https://source.unsplash.com/1600x900/nature",
+        "https://source.unsplash.com/collection/190727/1600x900",
+      ],
+      currentIndex: 0,
+    };
+  },
+  mounted: function() {
+    this.startSlide();
+  },
+
+  methods: {
+    startSlide: function() {
+      this.timer = setInterval(this.next, 6000);
+    },
+
+    next: function() {
+      this.currentIndex += 1;
+    },
+    prev: function() {
+      this.currentIndex -= 1;
+    },
+  },
+
+  computed: {
+    currentImg: function() {
+      return this.images[Math.abs(this.currentIndex) % this.images.length];
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="home">
+    <!-- Cards -->
+    <div class="home flex justify-center flex-col items-center">
+      <img
+        class="w-full h-80 object-cover hidden md:block"
+        src="https://source.unsplash.com/random"
+      />
+      <div class="container mx-5">
+        <div
+          class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-12 md:-mt-36 justify-items-center"
+        >
+          <div
+            class="flex flex-col items-center bg-white p-10 rounded-lg shadow-xl w-4/5 mt-10 relative"
+            v-for="(item, index) in array"
+            :key="index"
+          >
+            <img
+              class="lg:h-64 lg:w-64 sm:h-64 sm:w-64 md:h-60 md:w-60 rounded-full object-cover opacity-100 hover:opacity-30 relative"
+              :src="item.img"
+            />
+            <p class="text-gray-500 font-bold absolute top-1/3 opacity-0 ">{{ item.test }}</p>
+            <h5 class="text-2xl font-semibold">{{ item.title }}</h5>
+            <p class="text-center mt-4">{{ item.text }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- About -->
+    <about />
+  </div>
+</template>
+
+<script>
+import About from "./About.vue";
+export default {
+  components: { About },
+  name: "Home",
+  data() {
+    return {
+      array: [
+        {
+          img: "https://source.unsplash.com/random",
+          test: "Some text for test",
+          title: "Some img info",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsam deleniti consectetur pariatur, eius at eos repudiandae saepecorporis omnis asperiores dolore ratione recusandae in doloribus,itaque sunt. Alias, totam.",
+        },
+        {
+          img: "https://source.unsplash.com/random",
+          test: "Some text for test",
+          title: "Some img info",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsam deleniti consectetur pariatur, eius at eos repudiandae saepecorporis omnis asperiores dolore ratione recusandae in doloribus,itaque sunt. Alias, totam.",
+        },
+        {
+          img: "https://source.unsplash.com/random",
+          test: "Some text for test",
+          title: "Some img info",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsam deleniti consectetur pariatur, eius at eos repudiandae saepecorporis omnis asperiores dolore ratione recusandae in doloribus,itaque sunt. Alias, totam.",
+        },
+        {
+          img: "https://source.unsplash.com/random",
+          test: "Some text for test",
+          title: "Some img info",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsam deleniti consectetur pariatur, eius at eos repudiandae saepecorporis omnis asperiores dolore ratione recusandae in doloribus,itaque sunt. Alias, totam.",
+        },
+        {
+          img: "https://source.unsplash.com/random",
+          test: "Some text for test",
+          title: "Some img info",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsam deleniti consectetur pariatur, eius at eos repudiandae saepecorporis omnis asperiores dolore ratione recusandae in doloribus,itaque sunt. Alias, totam.",
+        },
+        {
+          img: "https://source.unsplash.com/random",
+          test: "Some text for test",
+          title: "Some img info",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsam deleniti consectetur pariatur, eius at eos repudiandae saepecorporis omnis asperiores dolore ratione recusandae in doloribus,itaque sunt. Alias, totam.",
+        },
+      ],
+    };
+  },
+};
+</script>
